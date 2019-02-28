e9b9dca8bbe128c21dd16aabd1c019a8dfa2b98f
Steve Jackson <sjackso@ixoreus.net>
2019-02-28 10:19:25 -0800

    Create a launchable entry script
    This brings up the UI, but nothing works yet

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
 Athena: A Toolkit for DNA Construction
+
+### Setting up a development environment ###
+
+How to run the project from a working directory:
+
+> virtualenv env
+> source env/activate
+> pip install PySide2
+> python src/athena.py
---
+++ src/athena.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+import sys
+
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow, QApplication, QLabel
+from PySide2.QtCore import QFile
+
+class AthenaWindow(QMainWindow):
+    def __init__( self, ui_file_path ):
+        super( AthenaWindow, self).__init__(None)
+        ui_file = QFile( ui_file_path )
+        ui_file.open( QFile.ReadOnly)
+
+        ui_loader = QUiLoader()
+        self.ui = ui_loader.load(ui_file)
+        ui_file.close()
+
+        self.statusLabel = self.ui.findChild(QLabel, "statusLabel")
+        self.ui.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = AthenaWindow('ui/AthenaMainWindow.ui')
+    sys.exit(app.exec_())
