f345b10dbd47c80e1b5bf6d368178240bb95a705
Steve Jackson <sjackso@ixoreus.net>
2019-03-07 10:20:59 -0800

    Include sample inputs in windows builds

--- build_win.bat
+++ build_win.bat
@@ -1,1 +1,1 @@
-pyinstaller .\src\athena.py --add-data "ui;ui" --add-data "tools;tools" --version-file version_info.txt %*
+pyinstaller .\src\athena.py --add-data "ui;ui" --add-data "tools;tools" --add-data "sample_inputs;sample_inputs" --version-file version_info.txt %*
