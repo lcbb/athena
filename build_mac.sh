e24692e65d4fe51a85b2a9402a49c79d166e89bc
Steve Jackson <sjackso@ixoreus.net>
2019-03-14 20:37:55 -0700

    Work around missing geometry loader plugin on mac

--- build_mac.sh
+++ build_mac.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-pyinstaller ./src/athena.py --add-data "ui:ui" --add-data "tools:tools" --add-data "sample_inputs:sample_inputs" $*
+pyinstaller ./src/athena.py --add-data "ui:ui" --add-data "tools:tools" --add-data "sample_inputs:sample_inputs" \
+                            --add-binary "${VIRTUAL_ENV}/lib/python3.7/site-packages/PySide2/Qt/plugins/geometryloaders:qt5_plugins/geometryloaders" $*
