d8bb6ebaad70682814749b5104b36f22023c4f10
Steve Jackson <sjackso@ixoreus.net>
2019-03-14 20:59:20 -0700

    Fix missing plugins on windows

--- build_win.bat
+++ build_win.bat
@@ -1,1 +1,3 @@
-pyinstaller .\src\athena.py --add-data "ui;ui" --add-data "tools;tools" --add-data "sample_inputs;sample_inputs" --version-file version_info.txt %*
+pyinstaller .\src\athena.py --add-data "ui;ui" --add-data "tools;tools" --add-data "sample_inputs;sample_inputs" ^
+                            --add-binary "%VIRTUAL_ENV%/Lib/site-packages/PySide2/plugins/geometryloaders;qt5_plugins/geometryloaders" ^
+                            --version-file version_info.txt %*
