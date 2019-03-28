b5e227da9bfd13a149a94f74724baf42c6add388
Steve Jackson <sjackso@ixoreus.net>
2019-03-27 17:09:37 -0700

    Fix deployment on mac too

--- build_mac.sh
+++ build_mac.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-pyinstaller ./src/athena.py --add-data "ui:ui" --add-data "tools:tools" --add-data "sample_inputs:sample_inputs" \
-                            --add-binary "${VIRTUAL_ENV}/lib/python3.7/site-packages/PySide2/Qt/plugins/geometryloaders:qt5_plugins/geometryloaders" $*
+pyinstaller ./src/main.py --add-data "ui:ui" --add-data "tools:tools" --add-data "sample_inputs:sample_inputs" \
+                          --add-data "src/qml:qml" --add-data "src/shaders:shaders" \
+                          --add-binary "${VIRTUAL_ENV}/lib/python3.7/site-packages/PySide2/Qt/plugins/geometryloaders:qt5_plugins/geometryloaders" \
+                          --name Athena $*
