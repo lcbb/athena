e144d24cd32259c037da5f2fbac9a617279153c0
Steve Jackson <sjackso@ixoreus.net>
2019-05-08 16:52:59 -0700

    Dynamically generate version artifacts on windows

--- build_preflight.py
+++ build_preflight.py
@@ -1,3 +1,24 @@
+import setuptools_scm
+import platform
+
+# Derive a version string from git
+athena_version = setuptools_scm.get_version()
+
+version_hunks = athena_version.split('.')
+ 
+print(version_hunks)
+
+assert( hunk.isdigit() for hunk in version_hunks[0:2] )
+
+def writeAthenaVersionFile():
+    template = 'version = "{}"\n'
+    content = template.format(athena_version)
+    filename = 'athena_version.py'
+    open(filename,'w+').write(content)
+    print('Wrote',filename)
+
+def writeWindowsVersionFile(version_hunks):
+    windows_version_template = '''
 # UTF-8
 #
 # For more details about fixed file info 'ffi' see:
@@ -6,8 +27,8 @@ VSVersionInfo(
   ffi=FixedFileInfo(
     # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
     # Set not needed items to zero 0.
-    filevers=(0, 0, 5, 0),
-    prodvers=(0, 0, 5, 0),
+    filevers=({0}, {1}, {2}, 0),
+    prodvers=({0}, {1}, {2}, 0),
     # Contains a bitmask that specifies the valid bits 'flags'r
     mask=0x3f,
     # Contains a bitmask that specifies the Boolean attributes of the file.
@@ -37,8 +58,17 @@ VSVersionInfo(
         StringStruct(u'LegalTrademarks', u''),
         StringStruct(u'OriginalFilename', u'Athena.exe'),
         StringStruct(u'ProductName', u'Athena'),
-        StringStruct(u'ProductVersion', u'0, 0, 5, 0')])
+        StringStruct(u'ProductVersion', u'{0}, {1}, {2}, 0')])
       ]), 
     #VarFileInfo([VarStruct(u'Translation', [1033, 1252])])
   ]
-)
+)'''
+    content = windows_version_template.format(*version_hunks)
+    filename = 'version_info.txt'
+    open(filename,'w+').write(content)
+    print("Wrote windows exe version info to", filename)
+
+
+writeAthenaVersionFile()
+if platform.system() ==  'Windows':
+    writeWindowsVersionFile(version_hunks)
