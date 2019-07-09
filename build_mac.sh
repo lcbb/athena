f7f673973ae15e3cf54b6bed2d85429b52b73bb3
Steve Jackson <sjackso@ixoreus.net>
2019-07-09 10:50:37 -0700

    Include about text in app builds

--- build_mac.sh
+++ build_mac.sh
@@ -8,7 +8,7 @@ fi
 VERSION=`python athena_version.py`
 
 pyinstaller ./src/main.py --add-data "ui:ui" --add-data "tools:tools" --add-data "sample_inputs:sample_inputs" \
-                          --add-data "src/qml:qml" --add-data "src/shaders:shaders" \
+                          --add-data "src/qml:qml" --add-data "src/shaders:shaders" --add-data "src/txt:txt" \
                           --add-data "athena_version.py:." \
                           --add-binary "${VIRTUAL_ENV}/lib/python3.7/site-packages/PySide2/Qt/plugins/geometryloaders:qt5_plugins/geometryloaders" \
                           --osx-bundle-identifier="edu.mit.lcbb.athena" \
--- build_win.bat
+++ build_win.bat
@@ -3,7 +3,7 @@ IF NOT EXIST "athena_version.py" (
     python .\build_preflight.py
 )
 pyinstaller .\src\main.py --add-data "ui;ui" --add-data "tools;tools" --add-data "sample_inputs;sample_inputs" ^
-                          --add-data "src/qml;qml" --add-data "src/shaders;shaders" ^
+                          --add-data "src/qml;qml" --add-data "src/shaders;shaders"  --add-data "src/txt;txt" ^
                           --add-data "athena_version.py;version.py" ^
                           --add-binary "%VIRTUAL_ENV%/Lib/site-packages/PySide2/plugins/geometryloaders;qt5_plugins/geometryloaders" ^
                           --version-file version_info.txt --name Athena --icon icon\athena.ico %*
